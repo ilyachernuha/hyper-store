1980b8af476088fdbecc4f53c4a4f92a93ae40db
Illia Chernukha <ilyachernuha@gmail.com>
2023-06-28 22:01:14 +0300

    add: minor changes

--- main.js
+++ main.js
@@ -2,7 +2,12 @@ const form = document.getElementById("form");
 
 form.addEventListener("submit", handleFormSubmit);
 
-function handleFormSubmit(e) {
+async function handleFormSubmit(e) {
   e.preventDefault();
-  console.log(e.target);
+  const { data } = await axios.post("http://127.0.0.1:3000", form, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  console.log(data);
 }
