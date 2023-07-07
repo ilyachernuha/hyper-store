3113ead697ff4d81103bd1a2d3ca3edc3907d8f1
Illia Chernukha <ilyachernuha@gmail.com>
2023-07-07 12:51:25 +0300

    fix: change vite base for dev and prod

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,6 @@ import { resolve } from "path";
 import handlebars from "vite-plugin-handlebars";
 
 export default {
-  base: "/hyper-store/",
   plugins: [
     handlebars({
       partialDirectory: resolve(__dirname, "components"),
