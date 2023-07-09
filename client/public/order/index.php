d42bd14dce3bee353b818d771252a5a24e93a2a1
Illia Chernukha <ilyachernuha@gmail.com>
2023-07-09 20:02:52 +0300

    fix: php

--- client/public/order/index.php
+++ client/public/order/index.php
@@ -1,47 +1,28 @@
-<!DOCTYPE html>
-<html lang="uk">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>
-      Cloud Stinger Core Wireless Gaming Headset + DTS | Hyper Store
-    </title>
-    <link
-      href="//row.hyperx.com/cdn/shop/files/HX_icon_PNG_72x.png?v=1670258278-72x72.png"
-      rel="apple-touch-icon"
-      sizes="72x72"
-    />
-    <link
-      href="//row.hyperx.com/cdn/shop/files/HX_icon_PNG_144x.png?v=1670258278-144x144.png"
-      rel="apple-touch-icon"
-      sizes="144x144"
-    />
-    <link
-      href="//row.hyperx.com/cdn/shop/files/HX_icon_PNG_196x.png?v=1670258278"
-      sizes="196x196"
-      rel="icon"
-      type="image/png"
-    />
-    <link
-      href="//row.hyperx.com/cdn/shop/files/HX_icon_PNG_96x.png?v=1670258278"
-      sizes="96x96"
-      rel="icon"
-      type="image/png"
-    />
-    <link
-      href="//row.hyperx.com/cdn/shop/files/HX_icon_PNG_32x.png?v=1670258278"
-      sizes="32x32"
-      rel="icon"
-      type="image/png"
-    />
-    <link
-      href="//row.hyperx.com/cdn/shop/files/HX_icon_PNG_16x.png?v=1670258278"
-      sizes="16x16"
-      rel="icon"
-      type="image/png"
-    />
-  </head>
-  <body>
-    <h1>Дякуємо, <?php echo $_POST["name"]; ?>! Замовлення передано. Менеджер зв'яжеться з вами протягом 10 хвилин.</h1>
-  </body>
-</html>
+<?php
+
+require_once '../../bot.php';
+
+$requestBody = file_get_contents('php://input');
+$data = json_decode($requestBody, true);
+
+if(isset($data['phone'])) {
+
+    $name = $data['name'];
+    $phone = $data['phone']; 
+    
+    $bot->sendMessage(
+        text: "Отримано нове замовлення номер #\nІм'я: {$name}\nНомер телефону: {$phone}",
+        chat_id: -1001986367819
+    );
+
+    $response = array('status' => 'success', 'message' => 'order received');
+    echo json_encode($response);
+
+} else {
+
+    $response = array('status' => 'error', 'message' => 'phone is required');
+    echo json_encode($response);
+    
+}
+
+?>
