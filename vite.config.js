e9492ad25d65ee5e11687d6a67d1ed0e85035bf5
Illia Chernukha <ilyachernuha@gmail.com>
2023-07-15 15:51:14 +0300

    fix: data-src resolve

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,21 @@ export default defineConfig({
     handlebars({
       partialDirectory: resolve(__dirname, "components"),
     }),
+    {
+      name: "lazysizes-data-src",
+      transformIndexHtml(html) {
+        const base = process.argv
+          .find((arg) => arg.startsWith("--base="))
+          ?.split("=")[1];
+        if (!base) {
+          return;
+        }
+        return html.replace(
+          new RegExp(/(data-src=)['"](.*)['"]/, "g"),
+          `$1"${base}$2"`
+        );
+      },
+    },
   ],
   build: {
     outDir: resolve(__dirname, "build/www"),
